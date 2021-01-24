e79b752f249a13913173d66692195875ebddf51b
Samyuel Danyo <samyueldanyo@gmail.com>
2021-01-24 17:31:08 +0000

    SRC: Round class added.

---
+++ src/include/rps_round.cc
@@ -0,0 +1,55 @@
+#include <thread>
+#include <future>
+# include <functional>
+
+#include "rps_round.h"
+#include "rps_player_computer.h"
+#include "rps_player_human.h"
+
+namespace rock_paper_scissors {
+    Round::Round(const int &round_id, PlayerHuman &round_player1,
+                 PlayerComputer &round_player2) :
+        id_(round_id), player1_(round_player1), player2_(round_player2) {
+        }
+
+        int
+        Round::GetId() {return id_;}
+
+        void
+        Round::Play() {
+            if(round_played_) {
+                Print({"Replaying round ", std::to_string(id_)});
+            }
+            auto future_symbol1 = std::async(std::bind(&PlayerHuman::MakeSymbol, std::ref(player1_)));
+            auto future_symbol2 = std::async(std::bind(&PlayerComputer::MakeSymbol, std::ref(player2_)));
+            symbol_player1_ = future_symbol1.get();
+            symbol_player2_ = future_symbol2.get();
+            auto winner_name = GetWinnerName(symbol_player1_, symbol_player2_);
+            if(winner_name == "player1") {
+                winner_ = std::make_shared<PlayerHuman>(player1_);
+                player1_.IncrementWins();
+            } else if(winner_name == "player2") {
+                winner_ = std::make_shared<PlayerComputer>(player2_);
+                player2_.IncrementWins();
+            } else if (winner_name == "none") {
+                Print({"Wrong symbol encountered!"});
+            }
+            round_played_ = true;
+        }
+
+        void
+        Round::Review() {
+            Print({"\n", ">>> Round ", std::to_string(id_), " Review <<<"});
+            Print({player1_.GetName(), " vs ", player2_.GetName()});
+            if(!round_played_) {
+                Print({"Round is not played yet. Come back later!", "\n"});
+                return;
+            }
+            Print({symbol_player1_, " vs ", symbol_player2_});
+            if(winner_ == nullptr) {
+                Print({"Outcome: Draw!", "\n"});
+                return;
+            }
+            Print({"Outcome: Winner: ", winner_->GetName(), "\n"});
+        }
+}
---
+++ src/include/rps_round.h
@@ -0,0 +1,26 @@
+#ifndef RPS_ROUND_H
+#define RPS_ROUND_H
+
+#include "rps_player_computer.h"
+#include "rps_player_human.h"
+
+namespace rock_paper_scissors {
+    class Round {
+      public:
+        Round(const int &round_id, PlayerHuman &round_player1,
+              PlayerComputer &round_player2);
+        int GetId();
+        void Play();
+        void Review();
+
+      private:
+        const int id_;
+        bool round_played_ {false};
+        PlayerHuman &player1_;
+        PlayerComputer &player2_;
+        std::string symbol_player1_;
+        std::string symbol_player2_;
+        std::shared_ptr<Player> winner_;
+    };
+}
+#endif // RPS_ROUND_H
