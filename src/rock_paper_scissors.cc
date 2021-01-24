91d157144b54fb0a0d8272a12d0e7def6eb3f89f
Samyuel Danyo <samyueldanyo@gmail.com>
2021-01-24 18:04:07 +0000

    SRC: Executable for the game added.

---
+++ src/rock_paper_scissors.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+#include "include/rps_game.h"
+#include "include/rps_util.h"
+
+int main() {
+    rock_paper_scissors::Print({"Welcome to Rock-Paper-Scissors!"});
+    std::string player_name;
+    char play_another_game;
+    rock_paper_scissors::Print({"Enter your name:"});
+    std::cin >> player_name;
+    do {
+        int number_of_rounds;
+        rock_paper_scissors::Print({"Enter number of game rounds:"});
+        std::cin >> number_of_rounds;
+        rock_paper_scissors::Game game(player_name, number_of_rounds);
+        game.Play();
+        game.Review();
+        rock_paper_scissors::Print({"Would you like to play another game?(y/n)"});
+        std::cin >> play_another_game;
+    } while(play_another_game == 'y');
+    return 0;
+}
